1c48137806f12c3a33c230ee4e987af6d569664b
xiongcen <xiongcen@xiongcendeiMac.local>
2016-07-04 18:39:44 +0800

    自定义波浪View 转自：http://blog.csdn.net/tianjian4592/article/details/44222565

---
+++ WaveViewDemo/src/main/java/com/example/xiongcen/myapplication/WaveViewActivity.java
@@ -0,0 +1,29 @@
+package com.example.xiongcen.myapplication;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+/**
+ * Created by xiongcen on 16-7-4.
+ */
+public class WaveViewActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.wave_demo_layout);
+
+        Button button = (Button) findViewById(R.id.button);
+        final WaveView view = (WaveView) findViewById(R.id.wave);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                view.startAnim();
+            }
+        });
+
+
+    }
+}
