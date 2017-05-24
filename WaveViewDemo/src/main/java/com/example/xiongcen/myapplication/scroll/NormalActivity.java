e17e463379b741999de4829964ec7a53f9c60072
xiongcen <xiongcen@xiongcendeiMac.local>
2017-05-24 21:01:21 +0800

    Android 向右滑动销毁（finish）Activity, 随着手势的滑动而滑动的效果 http://blog.csdn.net/xiaanming/article/details/20934541

---
+++ WaveViewDemo/src/main/java/com/example/xiongcen/myapplication/scroll/NormalActivity.java
@@ -0,0 +1,16 @@
+package com.example.xiongcen.myapplication.scroll;
+
+import android.os.Bundle;
+
+import com.example.xiongcen.myapplication.R;
+
+public class NormalActivity extends SwipeBackActivity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_normal);
+	}
+	
+
+}
---
+++ WaveViewDemo/src/main/java/com/example/xiongcen/myapplication/scroll/ScrollActivity.java
@@ -0,0 +1,15 @@
+package com.example.xiongcen.myapplication.scroll;
+
+import android.os.Bundle;
+
+import com.example.xiongcen.myapplication.R;
+
+public class ScrollActivity extends SwipeBackActivity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_scroll);
+	}
+
+}
